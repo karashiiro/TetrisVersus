678752f4b25619f4fc77bda4237229e17523b5f9
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-05-11 19:11:09 -0700

    Position next queue shapes correctly

--- Assets/Tetris/Queue.cs
+++ Assets/Tetris/Queue.cs
@@ -14,6 +14,7 @@ namespace Tetris
 
         private int head;
         private int tail;
+        private int count;
 
         public bool IsFull => incoming[tail] != null;
         public bool IsEmpty => incoming[head] == null;
@@ -22,20 +23,40 @@ namespace Tetris
         public BlockGroup Pop(Transform parent)
         {
             if (IsEmpty) return null;
+
             var next = incoming[head];
             incoming[head] = null;
             head = GetNextIndex(head);
+            count--;
+
             next.transform.SetParent(parent);
+            UpdatePositions();
             return next;
         }
 
         public bool Push(BlockGroup group)
         {
             if (IsFull) return false;
+
             incoming[tail] = group;
             tail = GetNextIndex(tail);
-            group.transform.SetParent(transform);
+            count++;
+
+            group.transform.SetParent(transform, false);
+            UpdatePositions();
             return true;
+        }
+
+        private void UpdatePositions()
+        {
+            Debug.Log($"Queue.UpdatePositions: head={head}, tail={tail}");
+
+            var i = head;
+            for (var j = 0; j < count; j++)
+            {
+                incoming[i].SetPosition(new Vector2(0, -j * 3));
+                i = GetNextIndex(i);
+            }
         }
 
         private int GetNextIndex(int currentIndex)
