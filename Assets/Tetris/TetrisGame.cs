624c46145666686ef78607b34243bd17564a1bd6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2024-05-18 18:42:15 -0700

    Implement basic ownership requests

--- Assets/Tetris/TetrisGame.cs
+++ Assets/Tetris/TetrisGame.cs
@@ -19,6 +19,8 @@ namespace Tetris
         private bool rotateLeftHeld;
         private bool rotateRightHeld;
 
+        [UdonSynced] private GameState currentState;
+
         [field: SerializeField] public PlayArea PlayArea { get; set; }
         [field: SerializeField] public TickDriver TickDriver { get; set; }
 
@@ -28,13 +30,36 @@ namespace Tetris
             if (TickDriver == null) Debug.LogError("TetrisGame.Awake: TickDriver is null.");
         }
 
-        private void Start()
+        public override void Interact()
         {
-            // Set the owner to the first player in the instance for now
-            var owner = Networking.GetOwner(gameObject);
-            if (owner == null) return;
+            if (Networking.IsOwner(gameObject))
+            {
+                Debug.Log("TetrisGame.Interact: Already owner, skipping ownership request");
+                InitGame(VRCPlayerApi.GetPlayerById(1));
+                return;
+            }
 
-            InitGame(owner);
+            // Interact is always triggered by the local player
+            Debug.Log("TetrisGame.Interact: Requesting ownership of game");
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        public override bool OnOwnershipRequest(VRCPlayerApi requestingPlayer, VRCPlayerApi requestedOwner)
+        {
+            // OnOwnershipRequest is triggered by the requesting player (who should always be the interact player?)
+            // The current owner still needs to approve the request after this event resolves.
+            var shouldRequest = currentState == GameState.NotStarted;
+            Debug.Log($"TetrisGame.OnOwnershipRequest: shouldRequest={shouldRequest}");
+            return shouldRequest;
+        }
+
+        public override void OnOwnershipTransferred(VRCPlayerApi player)
+        {
+            // Before transfer approval, this is invoked by the requesting player. Ownership can still be transferred
+            // back to the original owner if the request is rejected (this is still local to the requester). However,
+            // after transfer approval, this is invoked by all players except for the requester. We need to assume
+            // that the request succeeds, but be able to roll back safely if it doesn't.
+            InitGame(player);
         }
 
         private void InitGame(VRCPlayerApi player)
@@ -52,15 +77,16 @@ namespace Tetris
             SetGameState(GameState.Playing);
         }
 
-        public void SetGameState(GameState nextState)
+        private void SetGameState(GameState nextState)
         {
-            if (nextState == GameState.Playing)
+            switch (nextState)
             {
-                TickDriver.enabled = true;
-            }
-            else if (nextState == GameState.Stopped)
-            {
-                TickDriver.enabled = false;
+                case GameState.Playing:
+                    TickDriver.enabled = true;
+                    break;
+                case GameState.Stopped:
+                    TickDriver.enabled = false;
+                    break;
             }
         }
 
@@ -89,7 +115,7 @@ namespace Tetris
         /// <param name="args"></param>
         public override void InputUse(bool value, UdonInputEventArgs args)
         {
-            Debug.Log($"TetrisGame.InputUse: {args.handType} {args.floatValue} {args.boolValue} {value}");
+            if (!Networking.IsOwner(gameObject)) return;
 
             // For some reason, InputUse fires twice on KBM inputs, unlike in the editor. We use some
             // additional state to avoid re-rotating a block until the rotate button is released.
@@ -121,6 +147,8 @@ namespace Tetris
 
         public override void InputDrop(bool value, UdonInputEventArgs args)
         {
+            if (!Networking.IsOwner(gameObject)) return;
+
             // TODO: Use a different event for VR
             if (!value) return;
             PlayArea.ExchangeHold();
@@ -133,6 +161,8 @@ namespace Tetris
         /// <param name="args"></param>
         public override void InputJump(bool value, UdonInputEventArgs args)
         {
+            if (!Networking.IsOwner(gameObject)) return;
+
             if (!value) return;
             PlayArea.HardDrop();
         }
@@ -144,6 +174,8 @@ namespace Tetris
         /// <param name="args"></param>
         public override void InputMoveHorizontal(float value, UdonInputEventArgs args)
         {
+            if (!Networking.IsOwner(gameObject)) return;
+
             var sign = Math.Sign(value);
             if (sign == 0)
             {
@@ -162,6 +194,8 @@ namespace Tetris
         /// <param name="args"></param>
         public override void InputMoveVertical(float value, UdonInputEventArgs args)
         {
+            if (!Networking.IsOwner(gameObject)) return;
+
             const int down = -1;
 
             var direction = Math.Sign(value);
